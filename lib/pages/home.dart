124a921ec0a2c062b24ae65d19633102803627e7
Luca Bazzea <luca.bazzea@gmail.com>
2023-12-07 19:00:51 +0200

    Basic layout and container for text preview

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,19 +1,32 @@
 import "package:flutter/material.dart";
+import "package:marquee/marquee.dart";
 
 class Home extends StatelessWidget {
   const Home({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
         backgroundColor: Colors.blueGrey,
         body: SafeArea(
-            child: Center(
-                child: TextField(
-          autofocus: true,
-          decoration: InputDecoration(
-              enabledBorder: OutlineInputBorder(borderSide: BorderSide(color: Colors.white)),
-              focusedBorder: OutlineInputBorder(borderSide: BorderSide(color: Colors.lightBlue))),
-        ))));
+            child: Container(
+                padding: const EdgeInsets.symmetric(vertical: 50, horizontal: 20),
+                child: Column(children: [
+                  Row(children: [
+                    Expanded(
+                      child: Container(
+                          height: 100, width: 100, color: Colors.black, child: Text("Text")),
+                    )
+                  ]),
+                  const SizedBox(height: 50),
+                  const TextField(
+                    autofocus: true,
+                    decoration: InputDecoration(
+                        enabledBorder:
+                            OutlineInputBorder(borderSide: BorderSide(color: Colors.white)),
+                        focusedBorder:
+                            OutlineInputBorder(borderSide: BorderSide(color: Colors.lightBlue))),
+                  )
+                ]))));
   }
 }
