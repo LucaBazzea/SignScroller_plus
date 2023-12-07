4ec38f9ae988262f4f9071448d1255ca31cc1be2
Luca Bazzea <luca.bazzea@gmail.com>
2023-12-07 19:22:28 +0200

    Center

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -9,29 +9,26 @@ class Home extends StatelessWidget {
     return Scaffold(
         backgroundColor: Colors.blueGrey,
         body: SafeArea(
-            child: Container(
-                padding: const EdgeInsets.symmetric(vertical: 50),
+            child: Center(
                 child: Column(children: [
-                  Row(children: [
-                    Expanded(
-                      child: Container(
-                          height: 180,
-                          color: Colors.black,
-                          child: Marquee(
-                            text: "Text",
-                            style: const TextStyle(color: Colors.green, fontSize: 150),
-                          )),
-                    )
-                  ]),
-                  const SizedBox(height: 25),
-                  const TextField(
-                    autofocus: true,
-                    decoration: InputDecoration(
-                        enabledBorder:
-                            OutlineInputBorder(borderSide: BorderSide(color: Colors.white)),
-                        focusedBorder:
-                            OutlineInputBorder(borderSide: BorderSide(color: Colors.lightBlue))),
-                  )
-                ]))));
+          Row(children: [
+            Expanded(
+              child: Container(
+                  height: 180,
+                  color: Colors.black,
+                  child: Marquee(
+                    text: "Text",
+                    style: const TextStyle(color: Colors.green, fontSize: 150),
+                  )),
+            )
+          ]),
+          const SizedBox(height: 25),
+          const TextField(
+            autofocus: true,
+            decoration: InputDecoration(
+                enabledBorder: OutlineInputBorder(borderSide: BorderSide(color: Colors.white)),
+                focusedBorder: OutlineInputBorder(borderSide: BorderSide(color: Colors.lightBlue))),
+          )
+        ]))));
   }
 }
